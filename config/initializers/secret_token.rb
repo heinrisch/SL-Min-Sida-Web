91559b3d26e233f4ad22d821e4cda7e9f9b0406b
Henrik Sandström <henrik@wrapp.com>
2013-03-16 13:27:05 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SLOnRails::Application.config.secret_token = 'ea3eed6c89acf8cf71b917467dec5ab5f963686bb208590175ee3133735481a5a38ea0493e2530d1e519a60cf097103e37327eb5bf889ff16661cb0373e139c5'
